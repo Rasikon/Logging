feeefedaf1f22e9ac692ec6da487f628a650370f
Rasikon <redreev.ivan@gmail.com>
2018-12-12 16:58:54 +0500

    user servlets

--- child_module/src/main/java/hello/HelloServlet.java
+++ child_module/src/main/java/hello/HelloServlet.java
@@ -4,10 +4,18 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.*;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@WebServlet(urlPatterns = "/HelloWorldServlet", loadOnStartup = 1)
+@WebServlet(urlPatterns = "/HelloWorldServlet")
 public class HelloServlet extends HttpServlet {
+	static final Logger rootLogger = LogManager.getLogger(HelloServlet.class);
     private HelloWorld hello = null;
+	private User user = new User();
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -15,6 +23,23 @@ public class HelloServlet extends HttpServlet {
         hello = new HelloWorld();
         hello.start();
     }
+	
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+             req.getRequestDispatcher("myservlet.jsp").forward(req, resp);
+
+    }
+	
+	@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	user.stopActive();
+    String name = req.getParameter("name");
+	user = new User();
+	user.setNam(name);
+	user.start();
+	}
+	
 
     @Override
     public void destroy() {
---
+++ child_module/src/main/java/hello/User.java
@@ -0,0 +1,36 @@
+package hello;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class User extends Thread {
+    private boolean isActive = true;
+	public String nam = null;
+    static final Logger rootLogger = LogManager.getLogger(User.class);
+
+    public void run() {
+		this.isActive = true;
+        while (isActive) {
+            try {
+                Thread.sleep(1000);
+                rootLogger.info("Hello "+nam);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void stopActive() {
+        this.isActive = false;
+    }
+	
+
+	public String getNam() {
+        return nam;
+    }
+	
+
+    public void setNam(String nam) {
+        this.nam = nam;
+    }
+}
